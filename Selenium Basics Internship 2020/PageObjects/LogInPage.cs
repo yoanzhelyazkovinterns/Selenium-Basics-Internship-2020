1441c9abd90f6b600bc789557fb7d29409094ea8
yoanzhelyazkov <yoanzhelyazkov@gmail.com>
2020-03-23 18:18:30 +0200

    Added positive and negative Test scenarios.

---
+++ Selenium Basics Internship 2020/PageObjects/LogInPage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Selenium_Basics_Internship_2020
+{
+	class LogInPage
+	{
+		private IWebDriver driver;
+		readonly By userNameField = By.Id("usr");
+		readonly By passwordField= By.Id("pwd");
+		readonly By logInButton = By.CssSelector("[type='submit']");
+		readonly By welcomeMessage = By.CssSelector("div#case_login > .success");
+		readonly By errorMessage = By.CssSelector("div#case_login > .error");
+
+		public LogInPage(IWebDriver driver)
+		{
+			this.driver = driver;
+		}
+
+		public void EnterUserName(string userName)
+		{
+			driver.FindElement(userNameField).Clear();
+			driver.FindElement(userNameField).SendKeys(userName);
+		}
+
+		public void EnterPassword(string password)
+		{
+			driver.FindElement(passwordField).Clear();
+			driver.FindElement(passwordField).SendKeys(password);
+		}
+
+		public void ClickOnLogInButton()
+		{
+			driver.FindElement(logInButton).Click();
+		}
+
+		public bool IsLogInSuccessfull()
+		{
+			return driver.FindElement(welcomeMessage).Displayed;
+		}
+
+		public bool IsLogInErrorDispalyed()
+		{
+			return driver.FindElement(errorMessage).Displayed;
+		}
+	}
+}
---
+++ Selenium Basics Internship 2020/Tests/LogInTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Basics_Internship_2020
+{
+	class SearchTest
+	{
+		private ChromeDriver driver;
+		private readonly string homepage = "http://testing-ground.scraping.pro/login";
+
+		private readonly string correctUserName = "admin";
+		private readonly string correctPassword = "12345";
+
+		private readonly string wrongUserName = "user";
+		private readonly string wrongPassword = "54321";
+
+
+		[SetUp]
+		public void SetUp()
+		{
+			driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+		}
+
+		[TearDown]
+		public void CloseDriver()
+		{
+			driver.Quit();
+		}
+
+		public void NavigateToURL(string url)
+		{
+			driver.Url = url;
+		}
+
+		public IWebDriver GetDriver
+		{
+			get { return driver; }
+		}
+
+		[Test]
+		public void LogInSuccessfully()
+		{
+			NavigateToURL(homepage);
+
+			LogInPage logInPage = new LogInPage(driver);
+
+			logInPage.EnterUserName(correctUserName);
+			logInPage.EnterPassword(correctPassword);
+
+			logInPage.ClickOnLogInButton();
+
+			Assert.IsTrue(logInPage.IsLogInSuccessfull());
+		}
+
+		[Test]
+		public void LogInErrorMessageSuccessfullyDisplayed()
+		{
+			NavigateToURL(homepage);
+
+			LogInPage logInPage = new LogInPage(driver);
+
+			logInPage.EnterUserName(wrongUserName);
+			logInPage.EnterPassword(wrongPassword);
+
+			logInPage.ClickOnLogInButton();
+
+			Assert.IsTrue(logInPage.IsLogInErrorDispalyed());
+		}
+	}
+}
